c2137039f3617d478f1a5b9e74aa866ff4872422
ckjoris <2751776+ckjoris@users.noreply.github.com>
2024-09-08 16:29:15 +0300

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,115 @@
 // Node crypto & bufferFrom
-import {Buffer} from 'buffer'
-import {URL} from 'url'
-import * as crypto from 'crypto'
-import type {TUtils, TB64UrlEncode, TIsVerified, IPayload} from './index.d'
+import { Buffer } from "buffer";
+import { URL } from "url";
+import * as crypto from "crypto";
+
+export type TResult = {
+  verified: boolean;
+  message: string;
+  payload?: IPayload;
+  authObject?: Record<"header" | "payload" | "signature", string>;
+};
+
+export type TUtils = (a: string) => string;
+export type TB64UrlEncode = (a: Buffer) => string;
+export interface IPayload {
+  iss: string;
+  dest: string;
+  aud: string;
+  sub: string;
+  exp: number;
+  nbf: number;
+  iat: number;
+  jti: string;
+}
 
 // Utils
-const atob: TUtils = (a = '') => Buffer.from(a, 'base64').toString('binary')
-const base64UrlEncode: TB64UrlEncode = (buffer) => buffer.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '')
-
+const atob: TUtils = (a = "") => Buffer.from(a, "base64").toString("binary");
+const base64UrlEncode: TB64UrlEncode = (buffer) =>
+  buffer
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/g, "");
 
 // is Verified and not expired
-const isVerified: TIsVerified = (authorization: string, secret: string, key: string,  cb?: Function, returnCallback?: boolean ) => {
+export default function isVerified(
+  authorization: string,
+  secret: string,
+  key: string,
+): TResult {
   // Early return for missing params
-  if(!authorization || !secret || !key) { 
-    console.error('authorization or app secret or app key missing')
-    return false
+  if (!authorization || !secret || !key) {
+    return {
+      verified: false,
+      message: "authorization or app secret or app key missing",
+    };
   }
-  // probably could be cleaned up this is dirty, straight string replace to remove the stragglers and split it. 
-  const auth: string[] = authorization.replace('Bearer ', '').split('.')
+  // probably could be cleaned up this is dirty, straight string replace to remove the stragglers and split it.
+  const auth: string[] = authorization.replace("Bearer ", "").split(".");
   // will be passed to the optional call back
-  const authObject: Record<('header' | 'payload' | 'signature'), string> = {
+  const authObject: Record<"header" | "payload" | "signature", string> = {
     header: atob(auth[0]),
     payload: atob(auth[1]),
     signature: auth[2],
-  }
-  
-  const headerPayload: string = [auth[0], auth[1]].join('.')
-  const signedBuffer: Buffer = crypto.createHmac('sha256', secret).update(headerPayload).digest()
-  const isVerified: boolean = authObject.signature === base64UrlEncode(signedBuffer)
-  
-  if(!isVerified) {
-    console.error('Token is invalid')
-    return false
+  };
+
+  const headerPayload: string = [auth[0], auth[1]].join(".");
+  const signedBuffer: Buffer = crypto
+    .createHmac("sha256", secret)
+    .update(headerPayload)
+    .digest();
+  const isVerified: boolean =
+    authObject.signature === base64UrlEncode(signedBuffer);
+
+  if (!isVerified) {
+    return {
+      verified: false,
+      message: "Token is invalid",
+    };
   }
   // validate not expired
-  const payload: IPayload = JSON.parse(authObject.payload)
-  const time = new Date().getTime() / 1000
-  const isExpired: boolean = payload.exp <= time
-  const isValidAfter: boolean = payload.nbf <= time
+  const payload: IPayload = JSON.parse(authObject.payload);
+  const time = new Date().getTime() / 1000;
+  const isExpired: boolean = payload.exp <= time;
+  const isValidAfter: boolean = payload.nbf <= time;
   const iss = new URL(payload.iss).hostname;
   const dest = new URL(payload.dest).hostname;
 
   // still valid
-  if(isExpired) {
-    console.error('Token is expired')
-    return false
+  if (isExpired) {
+    return {
+      verified: false,
+      message: "Token is expired",
+    };
   }
 
   // valid from
-  if(!isValidAfter) {
-    console.error('Token is not yet valid');
-    return false
+  if (!isValidAfter) {
+    return {
+      verified: false,
+      message: "Token is not yet valid",
+    };
   }
 
-  if(iss != dest) {
-    console.error(`Token issuer ${iss} does not match the destination ${dest}`);
-    return false
+  if (iss != dest) {
+    return {
+      verified: false,
+      message: `Token issuer ${iss} does not match the destination ${dest}`,
+    };
   }
 
-  if(payload.aud != key) {
-      console.error('Token does not match the Shopify API Key');
-      return false
+  if (payload.aud != key) {
+    return {
+      verified: false,
+      message: "Token does not match the Shopify API Key",
+    };
   }
 
-  // call the optional callback with the authObject
-  if(cb && !returnCallback) {
-    // in chain call back, 
-    cb(authObject, payload)
-  }
-  // return valid or a callback if specified by returnCallback
-  return !returnCallback ? true : cb(authObject, payload)
+  return {
+    verified: true,
+    message: "Token is valid",
+    payload,
+    authObject,
+  };
 }
-export default isVerified
--- tests/isVerified.spec.ts
+++ tests/isVerified.spec.ts
@@ -1,72 +1,77 @@
-import isVerified from '../src/index'
-import createMock from '../utils/createMock'
+import isVerified from "../src/index";
+import createMock from "../utils/createMock";
 
-const mockCallback = jest.fn(x => {})
-
-
-test('Expect false when no params are parsed to the function', () => {
-    // @ts-ignore
-    const failed = isVerified()
-    expect(failed).toBe(false)
-})
-
-test('a valid request both authenticity & not expired', () => {
+test("Expect false when no params are parsed to the function", () => {
   // @ts-ignore
-  const {bearer, signature, secret, key, headerPayload} = createMock()
-  const verified = isVerified(bearer, secret, key)
-  expect(verified).toBe(true)
-})
+  const result = isVerified();
+  expect(result).toBeInstanceOf(Object);
+  expect(result.verified).toBe(false);
+});
 
-test('Test expired or incorrect tbf', () => {
+test("a valid request both authenticity & not expired", () => {
   // @ts-ignore
-  const {builtExpiredAuth, signature, secret, key,} = createMock()
-  const verified = isVerified(builtExpiredAuth, secret, key)
-  expect(verified).toBe(false)
-})
+  const { bearer, signature, secret, key, headerPayload } = createMock();
+  const result = isVerified(bearer, secret, key);
+  expect(result.verified).toBe(true);
+});
 
-test('a valid request with a token direct from the session token request ( EG : minus Bearer )', () => {
+test("Test expired or incorrect tbf", () => {
   // @ts-ignore
-  const {secret, sessionToken, key} = createMock()
-  const verified = isVerified(sessionToken, secret, key)
-  expect(verified).toBe(true)
-})
+  const { builtExpiredAuth, signature, secret, key } = createMock();
+  const result = isVerified(builtExpiredAuth, secret, key);
+  expect(result.verified).toBe(false);
+});
 
-test('a fradulent request', () => {
+test("a valid request with a token direct from the session token request ( EG : minus Bearer )", () => {
   // @ts-ignore
-  const {bearer, signature, secret, key, headerPayload} = createMock(60000, true)
-  const verified = isVerified(bearer, secret, key)
-  expect(verified).toBe(false)
-})
+  const { secret, sessionToken, key } = createMock();
+  const result = isVerified(sessionToken, secret, key);
+  expect(result.verified).toBe(true);
+});
 
-test('a fradulent request where the expirey is correct but the hashes are invalid', () => {
+test("a fradulent request", () => {
   // @ts-ignore
-  const {bearer, signature, secret, key, headerPayload} = createMock(null, true)
-  const verified = isVerified(bearer, secret, key)
-  expect(verified).toBe(false)
-})
+  const { bearer, signature, secret, key, headerPayload } = createMock(
+    60000,
+    true,
+  );
+  const result = isVerified(bearer, secret, key);
+  expect(result.verified).toBe(false);
+});
 
-test('an expired token should fail the verification', () => {
+test("a fradulent request where the expirey is correct but the hashes are invalid", () => {
   // @ts-ignore
-  const {bearer, signature, secret, key, headerPayload} = createMock(-100)
-  const verified = isVerified(bearer, secret, key)
-  expect(verified).toBe(false)
-})
+  const { bearer, signature, secret, key, headerPayload } = createMock(
+    null,
+    true,
+  );
+  const result = isVerified(bearer, secret, key);
+  expect(result.verified).toBe(false);
+});
 
-test('The call back is called', () => {
+test("an expired token should fail the verification", () => {
   // @ts-ignore
-  const {bearer, encodedSignature, secret, key, headerPayload, mockHeader, mockPayload} = createMock()
-  const verified = isVerified(bearer, secret, key, mockCallback)
-  expect(verified).toBe(true)
-  expect(mockCallback.mock.calls.length).toBe(1)
-  expect(JSON.parse(mockCallback.mock.calls[0][0].header)).toEqual(mockHeader)
-  expect(JSON.parse(mockCallback.mock.calls[0][0].payload)).toEqual(mockPayload)
-  expect(mockCallback.mock.calls[0][0].signature).toEqual(encodedSignature)
+  const { bearer, signature, secret, key, headerPayload } = createMock(-100);
+  const result = isVerified(bearer, secret, key);
+  expect(result.verified).toBe(false);
+});
 
-})
-
-test('The callback results are returned', () => {
-  const {bearer, encodedSignature, secret, key, headerPayload, mockHeader, mockPayload} = createMock()
-  const iterableCallback = (...any) => Object.assign({}, {...any[0]}, {...any[1]}, {callback:true})
-  const verified: any = isVerified(bearer, secret, key, iterableCallback, true)
-  expect(verified.callback).toBe(true)
-})
+test("returns payload", () => {
+  // @ts-ignore
+  const {
+    bearer,
+    encodedSignature,
+    secret,
+    key,
+    headerPayload,
+    mockHeader,
+    mockPayload,
+  } = createMock();
+  const result = isVerified(bearer, secret, key);
+  expect(result.verified).toBe(true);
+  expect(JSON.parse(result.authObject.header)).toEqual(mockHeader);
+  expect(JSON.parse(result.authObject.payload)).toEqual(
+    mockPayload,
+  );
+  expect(result.authObject.signature).toEqual(encodedSignature);
+});
